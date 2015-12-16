58ddc0cece9aff5da30ffdb6f4d1b4a9a6fa5142
Martin Broder <mrtnbroder@gmail.com>
2015-12-16 14:43:55 +0100

    add basic styles and material icons

--- app/views/IndexPage.jsx
+++ app/views/IndexPage.jsx
@@ -1,8 +1,9 @@
 
 import React, { Component, PropTypes as PT } from 'react'
 import { renderToStaticMarkup } from 'react-dom/server'
+import { default as radium, Style } from 'radium'
 
-export default class IndexPage extends Component {
+class IndexPage extends Component {
 
   static propTypes = {
     app: PT.string.isRequired,
@@ -16,6 +17,10 @@ export default class IndexPage extends Component {
   static renderToStaticMarkup(props) {
     return IndexPage.getDoctype()
       + renderToStaticMarkup(<IndexPage {...props}/>)
+  }
+
+  constructor(props, context) {
+    super(props, context)
   }
 
   render() {
@@ -50,7 +55,9 @@ export default class IndexPage extends Component {
           <meta content='true' name='HandheldFriendly'/>
           <meta content='320' name='MobileOptimized'/>
 
-          {/* <link rel='stylesheet' href={this.props.mainCSS}/> */}
+          {/* Material Design Icons */}
+          <link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet'/>
+          <Style rules={rules}/>
 
         </head>
         <body>
@@ -64,4 +71,48 @@ export default class IndexPage extends Component {
     )
   }
 
+}
+
+export default radium(IndexPage)
+
+const rules = {
+  '*, *:before, *:after': {
+    boxSizing: 'border-box',
+    margin: 0,
+    padding: 0,
+    lineHeight: 1
+  },
+  'html, body, #app, #app': {
+    height: '100%',
+    width: '100%'
+  },
+  'body': {
+    background: '#fff',
+    color: '#555',
+    fontFamily: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+    fontSize: '16px'
+  },
+  'h1': {
+    fontSize: '24px',
+    paddingBottom: '12px'
+  },
+  'p': {
+    lineHeight: 1.5,
+    paddingBottom: '12px'
+  },
+  'img': {
+    height: 'auto',
+    display: 'inline-block',
+    verticalAlign: 'middle',
+    maxWidth: '100%'
+  },
+  'input, textarea': {
+    'color': 'cadetblue',
+    'textShadow': '0px 0px 0px #000',
+    '-webkit-text-fill-color': 'transparent'
+  },
+  'input::-webkit-input-placeholder, textarea::-webkit-input-placeholder': {
+    'textShadow': 'none',
+    '-webkit-text-fill-color': 'initial'
+  }
 }
